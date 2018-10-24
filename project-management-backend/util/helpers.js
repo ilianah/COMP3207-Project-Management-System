0ffa0e8a8e6e93501c2d4032ab0298625b0ebc66
Iliana Hadzhiatanasova <ih1u16@soton.ac.uk>
2018-10-24 23:23:00 +0100

    More helper functions to simplify code

--- project-management-backend/util/helpers.js
+++ project-management-backend/util/helpers.js
@@ -1,10 +1,22 @@
-export function respondWithHeaders(statusCode, body) {
-    return {
-        statusCode, 
-        body: JSON.stringify(body),
-        headers: {
-            "Access-Control-Allow-Origin" : "*",
-            "Access-Control-Allow-Credentials" : true    
-        }
+let respondWithHeaders = (statusCode, body) => ({
+    statusCode,
+    body: JSON.stringify(body),
+    headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
     }
+});
+
+module.exports.respondWithHeaders = respondWithHeaders;
+
+module.exports.permissionError = () => {
+    return respondWithHeaders(401, { message: 'Invalid permissions' });
+}
+
+module.exports.validationError = (message) => {
+    console.log('validationError', message);
+    return respondWithHeaders(400, {message});
+}
+module.exports.hasRole = (event, role) => {
+    return (event.requestContext.authorizer.claims['cognito:groups']) === role
 }
