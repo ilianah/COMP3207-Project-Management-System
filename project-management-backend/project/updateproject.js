c8ec1c0b52887b0064adc3644e5beffd7216697e
Iliana Hadzhiatanasova <ih1u16@soton.ac.uk>
2018-10-26 17:23:21 +0100

    Moved input validation to helper file

--- project-management-backend/project/updateproject.js
+++ project-management-backend/project/updateproject.js
@@ -4,7 +4,7 @@ require
 let AWS = require('aws-sdk');
 let uuid = require('uuid');
 let documentClient = new AWS.DynamoDB.DocumentClient();
-let { respondWithHeaders, permissionError, validationError, hasRole } = require('../util/helpers');
+let { respondWithHeaders, permissionError, validationError, validationCheck, hasRole } = require('../util/helpers');
 
 module.exports.handler = async event => {
   if (!hasRole(event, 'Admin') && !hasRole(event, 'ProjectManager'))
@@ -15,20 +15,7 @@ module.exports.handler = async event => {
   if (!hasRole(event, 'Admin') && body.owner !== event.requestContext.authorizer.claims['cognito:username'])
     return validationError("The project you update must be yours");
 
-  if (!body.name || body.name.length === 0 || body.name.length > 80)
-    return validationError('Project name must be between 1 and 80 characters');
-
-  if (!body.description || body.description.length == 0 || body.description.length > 250)
-    return validationError('Project description must be between 1 and 250 characters');
-
-  if (!body.status || (body.status != 'New' && body.status != 'In progress' && body.status != "Completed"))
-    return validationError('Invalid project status');
-
-  if (!body.owner || body.owner.length === 0)
-    return validationError("Each project must have a project owner!");
-
-  if (!body.assignees || !(Array.isArray(body.assignees)) || body.assignees.length < 1)
-    return validationError("Invalid assignees");
+  validationCheck(body);
 
   try {
     let project = {
--- project-management-backend/util/helpers.js
+++ project-management-backend/util/helpers.js
@@ -7,16 +7,37 @@ let respondWithHeaders = (statusCode, body) => ({
     }
 });
 
+let validationError = (message) => {
+    console.log('validationError', message);
+    return respondWithHeaders(400, { message });
+}
+
 module.exports.respondWithHeaders = respondWithHeaders;
+
+module.exports.validationError = validationError;
 
 module.exports.permissionError = () => {
     return respondWithHeaders(401, { message: 'Invalid permissions' });
 }
 
-module.exports.validationError = (message) => {
-    console.log('validationError', message);
-    return respondWithHeaders(400, {message});
+module.exports.validationCheck = (body) => {
+
+    if (!body.name || body.name.length === 0 || body.name.length > 80)
+        return validationError('Project name must be between 1 and 80 characters');
+
+    if (!body.description || body.description.length == 0 || body.description.length > 250)
+        return validationError('Project description must be between 1 and 250 characters');
+
+    if (!body.status || (body.status != 'New' && body.status != 'In progress' && body.status != "Completed"))
+        return validationError('Invalid project status');
+
+    if (!body.owner || body.owner.length === 0)
+        return validationError("Each project must have a project owner!");
+
+    if (!body.assignees || !(Array.isArray(body.assignees)) || body.assignees.length < 1)
+        return validationError("Invalid assignees");
 }
+
 module.exports.hasRole = (event, role) => {
     return (event.requestContext.authorizer.claims['cognito:groups']) === role
 }
